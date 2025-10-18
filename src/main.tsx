9f18a6d28d6321ca059ff2ca154f4b34d01344ac
Seyed Mohammad Ahmadi <seyedmohammadahmadi2000@gmail.com>
2025-10-18 22:45:23 +0330

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,30 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/mo-commerce">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// Vite مقدار base را در این متغیر تزریق می‌کند
+// در dev = "/" و در GitHub Pages = "/mo-commerce/"
+const basename = import.meta.env.BASE_URL; // مثل "/mo-commerce/"
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mo-commerce">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
